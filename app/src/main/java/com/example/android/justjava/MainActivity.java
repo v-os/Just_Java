1cfd4e3819bca5c156847e2c4e295197748a8fd1
v-os <voidef@gmail.com>
2016-03-22 21:09:15 +0200

    2b That would be $" + quantity * 5 + " please."

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String  priceMessage = "Amount due $" + quantity * 5;
+        String  priceMessage = "That would be $" + quantity * 5 + " please.";
         displayMessage(priceMessage);
     }
     /**
